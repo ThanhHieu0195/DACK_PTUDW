8fc27859b15ea4c47ca59d0b1b20b8824c0bc2ee
quanghuy4994 <1312232@student.hcmus.edu.vn>
2016-05-30 23:58:22 +0700

    V0.2
    Thiết kế lại giao diện

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,22 @@
 /**
  * Created by quang on 5/30/2016.
  */
+
+var app = angular.module('hidrobook', ['ui.bootstrap']);
+app.controller('CarouselCtrl', function($scope, $http) {
+    $scope.myInterval = 3000;
+    $scope.slides = [
+        {
+            image: 'http://lorempixel.com/400/200/'
+        },
+        {
+            image: 'http://lorempixel.com/400/200/food'
+        },
+        {
+            image: 'http://lorempixel.com/400/200/sports'
+        },
+        {
+            image: 'http://lorempixel.com/400/200/people'
+        }
+    ];
+});
