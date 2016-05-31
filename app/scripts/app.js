2ec8d822079557161c5d2c660cbaaf58cab94a25
quanghuy4994 <1312232@student.hcmus.edu.vn>
2016-05-31 11:04:17 +0700

    V0.3
    Index

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,16 +7,16 @@ app.controller('CarouselCtrl', function($scope, $http) {
     $scope.myInterval = 3000;
     $scope.slides = [
         {
-            image: 'http://lorempixel.com/400/200/'
+            image: 'http://lorempixel.com/900/300/animals'
         },
         {
-            image: 'http://lorempixel.com/400/200/food'
+            image: 'http://lorempixel.com/900/300/food'
         },
         {
-            image: 'http://lorempixel.com/400/200/sports'
+            image: 'http://lorempixel.com/900/300/sports'
         },
         {
-            image: 'http://lorempixel.com/400/200/people'
+            image: 'http://lorempixel.com/900/300/people'
         }
     ];
 });
