96e547d20fb3634f8a74d72cd80e90fc3b19ba61
Hieu Thanh <thanhhieu0195@gmail.com>
2016-05-31 07:57:24 +0700

    show data book in web main

--- DACK_PTUDW/js/js.js
+++ DACK_PTUDW/js/js.js
@@ -15,6 +15,61 @@ app.controller('controlerapp', function($scope, $http){
 			   });
       			return books;
       		};
+
+      		$scope.filterbook = function(books, key, index){
+      			console.log(index);
+      			var book;
+      			var booksnew=[];
+      			switch(key){
+      				case 'new':
+	      				var knew = 0;
+	      				// lọc các sách mới phát hành
+	      				angular.forEach(books, function(value, key){
+	      					// lấy mã kt
+	      					var sdate = value.date;
+	      					var arrdate=sdate.split('/');
+	      					var kfe = parseInt(arrdate[2]+arrdate[1]);
+	      					if(knew==kfe){
+	      						booksnew.push(value);
+	      					}
+	      					else if (knew<kfe){
+	      						booksnew=[];
+	      						booksnew.push(value);
+	      						knew=kfe;
+	      					}
+
+				  		 });
+	      				break;
+      				case 'max':
+	      				var kmax = books[0].number;
+	      				// lọc các sách mới phát hành
+	      				angular.forEach(books, function(value, key){
+	      					// lấy mã kt
+	      					var keynumber = value.number;
+	      					if(kmax==keynumber){
+	      						booksnew.push(value);
+	      					}
+	      					else if (kmax>keynumber){
+	      						booksnew=[];
+	      						booksnew.push(value);
+	      						kmax=keynumber;
+	      					}
+
+				  		 });
+	      				break;
+	      				console.log(booksnew);
+	      			default:
+	      				booksnew=books;
+      				
+
+      			}
+  				// lấy sach
+      	// 		var d = new Date();
+   				// var n = d.getMilliseconds()%(booksnew.length);
+      			book = booksnew[index];
+      			return book;
+      		};        
+
         });
 
       
