4fd0aae4f50abb095887e3a4691c982c0545b3e1
quanghuy4994 <1312232@student.hcmus.edu.vn>
2016-06-07 22:58:41 +0700

    V0.6
    Load data cho chi tiết sách

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,52 @@
 //angular.module('myApp', ['ui.bootstrap']);
 var app = angular.module('hidrobook', ['ui.bootstrap', 'ngRoute', 'firebase']);
 
+app.factory('bookService', ['$firebaseObject', function ($firebaseArray) {
+    var ref = new Firebase("https://hidrobook.firebaseio.com/");
+    data = $firebaseArray(ref);
+    return data;
+}]);
+
+app.controller('b-detailController', function ($scope, bookService,$routeParams) {
+    var bookId = $routeParams.id;
+    $scope.data = bookService;
+
+    $scope.data.$loaded(function () {
+        var getAllBook = function (data) {
+
+            var genres = data.types;
+
+            var books=[];
+            for(var i = 0; i < genres.length; i++){
+                for(var j = 0; j < data.books[genres[i].key].data.length; j++){
+                    console.log(i);
+                    books.push(data.books[data.types[i].key].data[j]);
+                }
+            }
+            return books;
+        };
+
+        var getBookByID = function (books, id) {
+            for(var k = 0; k < books.length; k++){
+                if(books[k].id == id)
+                    return books[k];
+            }
+        };
+
+        var books = getAllBook($scope.data);
+        window.db = $scope.book = getBookByID(books, bookId);
+
+        if($scope.book.number > 0) {
+            $scope.stt = "Còn hàng";
+            $scope.status = "url('images/tick.png')";
+        }
+        else {
+            $scope.stt = "Hết hàng";
+            $scope.status = "url('images/publish_x.png')";
+        }
+    });
+});
+
 app.controller('adminapp', function($scope){
     $scope.book = {title:"null",author:"null", number:"null", date:"null", rating:"null", image:"null", cost:"null", type:""};
     $scope.contentBook = ["title", "author", "number", "date", "rating", "image", "cost", "type"];
@@ -107,7 +153,25 @@ app.controller('controlerapp', function($scope, $firebaseObject){
               console.error(err);
             });
 
-
+    $scope.normalizeString = function(str)
+    {
+        str = str.toLowerCase();
+        str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+        str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+        str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+        str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+        str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+        str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+        str = str.replace(/đ/g, "d");
+        //str= str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'| |\"|\&|\#|\[|\]|~|$|_/g,"-");
+        /* tìm và thay thế các kí tự đặc biệt trong chuỗi sang kí tự - */
+        //str= str.replace(/-+-/g,"-"); //thay thế 2- thành 1-
+        str = str.replace(/^\-+|\-+$/g, "");
+        str = str.split(' ').join('-');
+        //cắt bỏ ký tự - ở đầu và cuối chuỗi
+        str = str.toLowerCase();
+        return str;
+    };
 });
 
  app.controller('CarouselCtrl', function($scope, $firebaseObject) {
@@ -119,7 +183,7 @@ app.controller('controlerapp', function($scope, $firebaseObject){
                       $scope.myInterval = 4000;
                     // Initializing  slide rray  
                     $scope.slides = $scope.data.images;
-                    console.log($scope.slides);
+                    //console.log($scope.slides);
                 });
 
            
@@ -130,17 +194,16 @@ app.controller('controlerapp', function($scope, $firebaseObject){
 // Controller Ends here
 
 
-app.config(["$routeProvider", function($routeProvider) {
+app.config(["$routeProvider", "$locationProvider", function($routeProvider) {
     $routeProvider.when("/home", {
         templateUrl: "views/home.html", //accessing a certain html page that was created within views
-    }).when("/book-detail", {
+    }).when("/book-detail/:id/:title", {
         // the rest is the same for ui-router and ngRoute...
         templateUrl: "views/book-detail.html",
+        controller: 'b-detailController'
     }).when("/admin", {
         templateUrl: "views/admin.html",
     }).when("/checkout/cart", {
         templateUrl: "views/checkout-cart.html",
-    }).otherwise({
-        redirectTo: '/home'
     });
 }]);
